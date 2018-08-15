4332fe66425ffa059c793e286c9d97e33361551c
Paulo Henrique Ortolan <paulo.ortolan@serpro.gov.br>
2018-08-15 11:49:27 -0300

    Renaming class

---
+++ src/main/java/org/pho/splitter/beans/BooleanSplit.java
@@ -0,0 +1,10 @@
+package org.pho.splitter.beans;
+
+import org.pho.splitter.function.SplitFunction;
+
+public class BooleanSplit implements SplitFunction<BooleanSplit> {
+
+    @Override public BooleanSplit split(String source) {
+        return null;
+    }
+}
