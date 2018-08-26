4c6082470b0e1f0bc7fcc64ab7d9ad8b09faa4bc
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2018-08-26 14:05:22 -0300

    IntegerSplitTest draft.

---
+++ src/test/java/org/pho/splitter/core/splits/IntegerSplitTest.java
@@ -0,0 +1,15 @@
+package org.pho.splitter.core.splits;
+
+import org.junit.jupiter.api.Test;
+import org.pho.splitter.core.exception.SplitterException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegerSplitTest {
+
+    @Test
+    void positiveInteger() throws SplitterException {
+        assertEquals(1, 1);
+    }
+
+}
