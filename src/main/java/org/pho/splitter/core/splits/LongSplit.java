5f751efe629a98fa1c4d767f7befe2c2007eb267
Paulo Henrique Ortolan <paulo.ortolan@gmail.com>
2018-09-09 19:42:14 -0300

    Adding LongSplit test

--- src/main/java/org/pho/splitter/core/splits/LongSplit.java
+++ src/main/java/org/pho/splitter/core/splits/LongSplit.java
@@ -2,6 +2,21 @@ package org.pho.splitter.core.splits;
 
 public class LongSplit extends AbstractSplitter<Long> {
 
+    private String[] specialCharacters;
+
+    private LongSplit(int start, int end, String... specialCharacters) {
+        setStart(start);
+        setEnd(end);
+    }
+
+    public static LongSplit newLongSplit(int start, int end, String... specialCharacters) {
+        return new LongSplit(start, end, specialCharacters);
+    }
+
+    public static LongSplit newLongSplit(int start, String... specialCharacters) {
+        return new LongSplit(start, END_OF_LINE, specialCharacters);
+    }
+
     @Override public Long split(String source) {
         return null;
     }
--- src/test/java/org/pho/splitter/core/splits/LongSplitTest.java
+++ src/test/java/org/pho/splitter/core/splits/LongSplitTest.java
@@ -1,10 +1,95 @@
 package org.pho.splitter.core.splits;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.pho.splitter.core.exception.SplitterException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LongSplitTest {
 
-//       -9223372036854775808
-//        9223372036854775807
+    private static final String DATA = "         36854775807        -36854775808 9223372036854775807-9223372036854775808LONG NUMBER INDEED!! 9300000000000000000";
+
+    private static final String NO_LIMIT_DATA = "         36854775807        -36854775808";
+
+    private static final String FORMATTED_DATA = "      36,854,775,807     -36,854,775,808";
+
+    @Test
+    @Disabled
+    @DisplayName("Long split test positive number")
+    public void positiveLong() throws SplitterException {
+        long actual = LongSplit.newLongSplit(0, 11).split(DATA);
+        long expected = 36_854_775_807L;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Long split test negative number")
+    public void negativeShort() throws SplitterException {
+        long actual = LongSplit.newLongSplit(11, 22).split(DATA);
+        long expected = -36_854_775_808L;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Long split test max")
+    public void maximumLongNumber() throws SplitterException {
+        long actual = LongSplit.newLongSplit(22, 33).split(DATA);
+        long expected = Long.MAX_VALUE;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Long split test min")
+    public void minimumLongNumber() throws SplitterException {
+        long actual = LongSplit.newLongSplit(33, 44).split(DATA);
+        long expected = Long.MIN_VALUE;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Long split exception String information")
+    public void notAnLong() {
+        assertThrows(SplitterException.class, () -> LongSplit.newLongSplit(44, 55).split(DATA));
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Long split exception out of range")
+    public void outOfRange() {
+        assertThrows(SplitterException.class, () -> LongSplit.newLongSplit(55).split(DATA));
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Long split no upper limit")
+    public void noLimitLong() throws SplitterException {
+        long actual = LongSplit.newLongSplit(11).split(NO_LIMIT_DATA);
+        long expected = -36_854_775_808L;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Formatted positive Long split test")
+    public void formattedPositiveLong() throws SplitterException {
+        long actual = LongSplit.newLongSplit(0, 11, ",").split(FORMATTED_DATA);
+        long expected = 36_854_775_807L;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Formatted negative Long split test")
+    public void formattedNegativeLong() throws SplitterException {
+        long actual = LongSplit.newLongSplit(11, ",").split(FORMATTED_DATA);
+        long expected = -36_854_775_808L;
+        assertEquals(expected, actual);
+    }
 
 }
